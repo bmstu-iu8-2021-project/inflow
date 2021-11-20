cd90cbb16b12ecaec1cb54ac7d194d915c7766cb
OlgaOlga29 <kalashnikova661@mail.ru>
2021-11-20 23:20:20 +0300

    try except

--- src/routes/resources/service.py
+++ src/routes/resources/service.py
@@ -12,75 +12,96 @@ class ResourceService(metaclass=Singleton):
         self.pool = pool
 
     def status(self):
-        conn = self.pool.getconn()
-        cursor = conn.cursor()
+        try:
+            conn = self.pool.getconn()
+            cursor = conn.cursor()
 
-        cursor.execute("SELECT count(id) FROM resources;")
-        
-        count = int(cursor.fetchone()[0])
+            cursor.execute("SELECT count(id) FROM resources;")
+            
+            count = int(cursor.fetchone()[0])
 
-        cursor.close()
-        self.pool.putconn(conn)
+            cursor.close()
+            self.pool.putconn(conn)
 
-        return {
-            "status": count,
-        }
+            return {
+                "status": count,
+            }
+        except:
+            return "oops"
 
 
     def search_by_tag(self, tags):
-        conn = self.pool.getconn()
-        cursor = conn.cursor()
-        
-        cursor.execute("SELECT resources.id, resources.title, resources.link FROM resources JOIN tags_resources WHERE tags_resources.tag_id IN %s;",(tags))
-        result = [Resource(*row).__dict__ for row in cursor.fetchall()]
-        cursor.close()
-        self.pool.putconn(conn)
-        return result
+        try:
+            conn = self.pool.getconn()
+            cursor = conn.cursor()
+            
+            cursor.execute("SELECT resources.id, resources.title, resources.link FROM resources JOIN tags_resources WHERE tags_resources.tag_id IN %s;",(tags))
+            result = [Resource(*row).__dict__ for row in cursor.fetchall()]
+            cursor.close()
+            self.pool.putconn(conn)
+            return result
+        except:
+            return "oops"
 
     def search_by_title(self, title):
-        conn = self.pool.getconn()
-        cursor = conn.cursor()
+        try:
+            conn = self.pool.getconn()
+            cursor = conn.cursor()
 
-        cursor.execute("SELECT id, title, link FROM resources WHERE title LIKE '%(%s)%';",(title))
-        result = [Resource(*row).__dict__ for row in cursor.fetchall()]
-        cursor.close()
-        self.pool.putconn(conn)
-        return result
+            cursor.execute("SELECT id, title, link FROM resources WHERE title LIKE '%(%s)%';",(title))
+            result = [Resource(*row).__dict__ for row in cursor.fetchall()]
+            cursor.close()
+            self.pool.putconn(conn)
+            return result
+        except:
+            return "oops"
 
     def delete(self, id):
-        conn = self.pool.getconn()
-        cursor = conn.cursor()
-        cursor.execute("DELETE id, title, link FROM resources WHERE id = %s;",(id))
-        cursor.execute("DELETE tag_id, resource_id FROM tags_resources WHERE resource_id = %s;",(id))
-        conn.commit()
-        cursor.close()
-        self.pool.putconn(conn)
+        try:
+            conn = self.pool.getconn()
+            cursor = conn.cursor()
+            cursor.execute("DELETE id, title, link FROM resources WHERE id = %s;",(id))
+            cursor.execute("DELETE tag_id, resource_id FROM tags_resources WHERE resource_id = %s;",(id))
+            conn.commit()
+            cursor.close()
+            self.pool.putconn(conn)
+        except:
+            return "oops"
 
     def create(self, title, link):
-        conn = self.pool.getconn()
-        cursor = conn.cursor()
-        cursor.execute("INSERT INTO resources(title, link) VALUES (%s, %s);", (title, link))
-        result = [Resource(*row).__dict__ for row in cursor.fetchall()]
-        conn.commit()
-        cursor.close()
-        self.pool.putconn(conn)
-        return result
+        try:
+            conn = self.pool.getconn()
+            cursor = conn.cursor()
+            cursor.execute("INSERT INTO resources(title, link) VALUES (%s, %s);", (title, link))
+            result = [Resource(*row).__dict__ for row in cursor.fetchall()]
+            conn.commit()
+            cursor.close()
+            self.pool.putconn(conn)
+            return result
+        except:
+            return "oops"
 
     def update_add_tags(self, tag_id, resource_id):
-        conn = self.pool.getconn()
-        cursor = conn.cursor()
-        cursor.execute("INSERT INTO tags_resources(tag_id, resource_id) VALUES (%s, %s);",(tag_id, resource_id))
-        conn.commit()
-        cursor.close()
-        self.pool.putconn(conn)
+        try:
+            conn = self.pool.getconn()
+            cursor = conn.cursor()
+            cursor.execute("INSERT INTO tags_resources(tag_id, resource_id) VALUES (%s, %s);",(tag_id, resource_id))
+            conn.commit()
+            cursor.close()
+            self.pool.putconn(conn)
+        except:
+            return "oops"
 
     def update_delete_tags(self, tag_id, resource_id):
-        conn = self.pool.getconn()
-        cursor = conn.cursor()
-        cursor.execute("DELETE resources_id, tag_id FROM tags_resources WHERE tag_id = %s AND resource_id = %s;",(tag_id, resource_id))
-        conn.commit()
-        cursor.close()
-        self.pool.putconn(conn)
+        try:
+            conn = self.pool.getconn()
+            cursor = conn.cursor()
+            cursor.execute("DELETE resources_id, tag_id FROM tags_resources WHERE tag_id = %s AND resource_id = %s;",(tag_id, resource_id))
+            conn.commit()
+            cursor.close()
+            self.pool.putconn(conn)
+        except:
+            return "oops"
 
     # def update(self):
     #     cursor = self.connection.cursor()
--- src/routes/tags/service.py
+++ src/routes/tags/service.py
@@ -15,30 +15,36 @@ class TagService(metaclass=Singleton):
         
 
     def all(self):
-        conn = self.pool.getconn()
-        cursor = conn.cursor()
-        cursor.execute("SELECT id, label, color FROM tags;")
-        # cursor.execute("CREATE FUNCTION all(code text) RETURNS table (id integer, label varchar, color varchar) as $$ BEGIN return query SELECT id, label, color FROM tags; EXCEPTION WHEN others THEN RAISE NOTICE 'SQLSTATE: %', SQLSTATE; RAISE; END; $$;")
-        # cursor.execute("CREATE FUNCTION all(out id int, out label text, out color text) RETURNS TABLE (id integer, label varchar, color varchar) as $$ DECLARE r record; BEGIN return query SELECT id, label, color INTO r FROM tags; RETURN QUERY SELECT id, label, color FROM tags; EXCEPTION WHEN others THEN RAISE NOTICE 'SQLSTATE: %', SQLSTATE; RAISE; END; $$;")
-        # cursor.execute("DO $$ DECLARE rec record; BEGIN SELECT id, label, color INTO rec FROM tags; raise notice '% % %', rec.id, rec.label, rec.color; END; $$ language plpgsql;")
-        result = [Tag(*row).__dict__ for row in cursor.fetchall()]
-        cursor.close()
-        self.pool.putconn(conn)
-        return result
+        try:
+            conn = self.pool.getconn()
+            cursor = conn.cursor()
+            cursor.execute("SELECT id, label, color FROM tags;")
+            # cursor.execute("CREATE FUNCTION all(code text) RETURNS table (id integer, label varchar, color varchar) as $$ BEGIN return query SELECT id, label, color FROM tags; EXCEPTION WHEN others THEN RAISE NOTICE 'SQLSTATE: %', SQLSTATE; RAISE; END; $$;")
+            # cursor.execute("CREATE FUNCTION all(out id int, out label text, out color text) RETURNS TABLE (id integer, label varchar, color varchar) as $$ DECLARE r record; BEGIN return query SELECT id, label, color INTO r FROM tags; RETURN QUERY SELECT id, label, color FROM tags; EXCEPTION WHEN others THEN RAISE NOTICE 'SQLSTATE: %', SQLSTATE; RAISE; END; $$;")
+            # cursor.execute("DO $$ DECLARE rec record; BEGIN SELECT id, label, color INTO rec FROM tags; raise notice '% % %', rec.id, rec.label, rec.color; END; $$ language plpgsql;")
+            result = [Tag(*row).__dict__ for row in cursor.fetchall()]
+            cursor.close()
+            self.pool.putconn(conn)
+            return result
+        except:
+            return "oops"
 
     def create(self, label, color):
-        conn = self.pool.getconn()
-        cursor = conn.cursor()
-        cursor.execute("DO $$ BEGIN INSERT INTO tags (label, color) VALUES (%s, %s) RETURNING id; EXCEPTION WHEN unique_violation THEN RAISE NOTICE 'Illegal operation: %', SQLERRM; END $$;", (label, color))
-        id = cursor.fetchone()[0]
-        
-        conn.commit()
-        
-        tag = Tag(id, label, color)
-        cursor.close()
-        self.pool.putconn(conn)
-        return [Tag(*row).__dict__ for row in cursor.fetchall()]
-        # return tag.__dict__
+        try:
+            conn = self.pool.getconn()
+            cursor = conn.cursor()
+            cursor.execute("DO $$ BEGIN INSERT INTO tags (label, color) VALUES (%s, %s) RETURNING id; EXCEPTION WHEN unique_violation THEN RAISE NOTICE 'Illegal operation: %', SQLERRM; END $$;", (label, color))
+            id = cursor.fetchone()[0]
+            
+            conn.commit()
+            
+            tag = Tag(id, label, color)
+            cursor.close()
+            self.pool.putconn(conn)
+            return [Tag(*row).__dict__ for row in cursor.fetchall()]
+            # return tag.__dict__
+        except:
+            return "oops"
         
     # def update(self, id, edit_params):
     #     conn = self.pool.getconn()
@@ -46,29 +52,38 @@ class TagService(metaclass=Singleton):
     #     cursor.execute("UPDATE tags SET label = %s, color = %s WHERE id = %s",())
 
     def delete(self, id):
-        conn = self.pool.getconn()
-        cursor = conn.cursor()
-        cursor.execute("DELETE id, label, color FROM tags WHERE id = %s;",(id))
-        cursor.execute("DELETE resources_id, tag_id FROM tags_resources WHERE tag_id = %s;",(id))
-        conn.commit()
-        cursor.close()
-        self.pool.putconn(conn)
+        try:
+            conn = self.pool.getconn()
+            cursor = conn.cursor()
+            cursor.execute("DELETE id, label, color FROM tags WHERE id = %s;",(id))
+            cursor.execute("DELETE resources_id, tag_id FROM tags_resources WHERE tag_id = %s;",(id))
+            conn.commit()
+            cursor.close()
+            self.pool.putconn(conn)
+        except:
+            return "oops"
 
     def search(self, label):
-        conn = self.pool.getconn()
-        cursor = conn.cursor()
-        cursor.execute("SELECT id, label, color FROM tags WHERE label LIKE '%(%s)%';",(label))
-        result = [Tag(*row).__dict__ for row in cursor.fetchall()]
-        conn.commit()
-        cursor.close()
-        self.pool.putconn(conn)
-        return result
+        try:
+            conn = self.pool.getconn()
+            cursor = conn.cursor()
+            cursor.execute("SELECT id, label, color FROM tags WHERE label LIKE '%(%s)%';",(label))
+            result = [Tag(*row).__dict__ for row in cursor.fetchall()]
+            conn.commit()
+            cursor.close()
+            self.pool.putconn(conn)
+            return result
+        except:
+            return "oops"
 
     def join(self, id_src, id_dst):
-        conn = self.pool.getconn()
-        cursor = conn.cursor()
-        cursor.execute("UPDATE tags_resources SET tag_id = %s WHERE tag_id = %s;",(id_dst, id_src))
-        cursor.execute("DELETE id, label, color FROM tags WHERE id = %s;",(id_src)) #другой тег удаляем
-        conn.commit()
-        cursor.close()
-        self.pool.putconn(conn)
+        try:
+            conn = self.pool.getconn()
+            cursor = conn.cursor()
+            cursor.execute("UPDATE tags_resources SET tag_id = %s WHERE tag_id = %s;",(id_dst, id_src))
+            cursor.execute("DELETE id, label, color FROM tags WHERE id = %s;",(id_src)) #другой тег удаляем
+            conn.commit()
+            cursor.close()
+            self.pool.putconn(conn)
+        except:
+            return "oops"
